e82444daaaa244e5c773ac0fbd5643df48419a82
anthonybx <111912318+anthonybx@users.noreply.github.com>
2022-11-01 18:41:04 +0100

    Update txt2img.py

--- modules/txt2img.py
+++ modules/txt2img.py
@@ -8,64 +8,35 @@ from modules.ui import plaintext_to_html
 
 def txt2img(prompt: str, negative_prompt: str, prompt_style: str, prompt_style2: str, steps: int, sampler_index: int, restore_faces: bool, tiling: bool, n_iter: int, batch_size: int, cfg_scale: float, seed: int, subseed: int, subseed_strength: float, seed_resize_from_h: int, seed_resize_from_w: int, seed_enable_extras: bool, height: int, width: int, enable_hr: bool, denoising_strength: float, firstphase_width: int, firstphase_height: int, *args):
     prompt = prompt.lower()    
-    prompt = prompt.replace("ramzan kadyrov", "man")
-    prompt = prompt.replace("ramzan", "man")
-    prompt = prompt.replace("kadyrov", "man")
-    prompt = prompt.replace("missile", "flower")
-    prompt = prompt.replace("nuclear", "flower")
-    prompt = prompt.replace("bomb", "flower")
-    prompt = prompt.replace("nuke", "flower")
-    prompt = prompt.replace("war", "love")
-    prompt = prompt.replace("путин", "dictator")
-    prompt = prompt.replace("putine", "dictator")
-    prompt = prompt.replace("poutine", "dictator")
-    prompt = prompt.replace("putin", "dictator")
-    prompt = prompt.replace("poutin", "dictator")
-    prompt = prompt.replace("little girl", "woman")
-    prompt = prompt.replace("little boy", "man")
-    prompt = prompt.replace("5 year old", "adult")
-    prompt = prompt.replace("6 year old", "adult")
-    prompt = prompt.replace("7 year old", "adult")
-    prompt = prompt.replace("8 year old", "adult")
-    prompt = prompt.replace("9 year old", "adult")
-    prompt = prompt.replace("10 year old", "adult")
-    prompt = prompt.replace("11 year old", "adult")
-    prompt = prompt.replace("12 year old", "adult")
-    prompt = prompt.replace("13 year old", "adult")
-    prompt = prompt.replace("14 year old", "adult")
-    prompt = prompt.replace("15 year old", "adult")
-    prompt = prompt.replace("16 year old", "adult")
-    prompt = prompt.replace("17 year old", "adult")
+    if ("ramzan kadyrov" in prompt) or ("ramzan" in prompt) or ("kadyrov" in prompt):
+      steps = 1
+    if ("missile" in prompt) or ("nuclear" in prompt) or ("bomb" in prompt) or ("nuke" in prompt):
+      steps = 1
+    if ("путин" in prompt) or ("putine" in prompt) or ("poutine" in prompt) or ("putin" in prompt) or ("poutin" in prompt):
+      steps = 1
+    if ("little girl" in prompt) or ("little boy" in prompt) or ("poutine" in prompt) or ("poutin" in prompt):
+      steps = 1
     if ("year" in prompt) and ("old" in prompt) and ("boy" in prompt):
       steps = 1
     if ("year" in prompt) and ("old" in prompt) and ("girl" in prompt):
       steps = 1
     if ("young" in prompt) and ("girl" in prompt):
-      prompt = "a pig"
       steps = 1
     if ("young" in prompt) and ("boy" in prompt):
-      prompt = "a pig"
       steps = 1
     if ("little" in prompt) and ("girl" in prompt):
-      prompt = "a pig"
       steps = 1
     if ("little" in prompt) and ("boy" in prompt):
-      prompt = "a pig"
       steps = 1
     if ("young" in prompt) and ("loli" in prompt):
-      prompt = "a pig"
       steps = 1
     if ("nude" in prompt) and ("young" in prompt):
-      prompt = "a pig"
       steps = 1
     if ("naked" in prompt) and ("young" in prompt):
-      prompt = "a pig"
       steps = 1
     if ("slutty" in prompt) and ("young" in prompt):
-      prompt = "a pig"
       steps = 1
     if ("chubby" in prompt):
-      steps = 1
       prompt = "a pig"
     if ("fat" in prompt) and ("woman" in prompt):
       steps = 1
@@ -74,72 +45,50 @@ def txt2img(prompt: str, negative_prompt: str, prompt_style: str, prompt_style2:
     if ("obese" in prompt):
       steps = 1
     if ("loli" in prompt):
-      prompt = "a pig"
       steps = 1
     if ("plus-size" in prompt):
-      prompt = "a pig"
-      steps = 1
-    if ("dragoness" in prompt):
-      prompt = "a pig"
       steps = 1
     if ("bbw" in prompt):
-      prompt = "a pig"
       steps = 1
     if ("chibi" in prompt) and ("girl" in prompt):
-      prompt = "a pig"
       steps = 1
     if ("fat" in prompt) and ("overweight" in prompt):
-      prompt = "a pig"
       steps = 1
     if ("teen" in prompt) and ("boy" in prompt):
-      prompt = "a pig"
       steps = 1
     if ("teen" in prompt) and ("girl" in prompt):
-      prompt = "a pig"
       steps = 1
     if ("bodybuilder" in prompt) and ("female" in prompt):
-      prompt = "a man imprisoned"
       steps = 1
     if ("bodybuilder" in prompt) and ("girl" in prompt):
-      prompt = "a man imprisoned"
       steps = 1
     if ("bodybuilder" in prompt) and ("feminine" in prompt):
-      prompt = "a man imprisoned"
       steps = 1
     if ("bodybuilder" in prompt) and ("woman" in prompt):
-      prompt = "a stupid guy imprisoned in jail,he is sad because his life is trash"
-    prompt = prompt.replace("overweight", "beautiful")
-    prompt = prompt.replace("sergei kuzhugetovich shoigu", "man")
+      steps = 1
+    if ("sergei kuzhugetovich shoigu" in prompt):
+      steps = 1
     if ("sergei" in prompt) and ("shoigu" in prompt):
-      prompt = "slavia ukraine"
+      steps = 1
     if ("kuzhugetovich" in prompt):
-      prompt = "slavia ukraine"
+      steps = 1
     if ("boy" in prompt) and ("cute" in prompt):
-      prompt = "a man imprisoned in jail"
       steps = 1
     if ("boy" in prompt) and ("handsome" in prompt):
-      prompt = "a man imprisoned in jail"
       steps = 1
     if ("shemale" in prompt) or ("hermaphrodite" in prompt):
-      prompt = "a man imprisoned in jail"
       steps = 1
     if ("muscular" in prompt) and ("girl" in prompt):
-      prompt = "a man imprisoned in jail"
       steps = 1
     if ("muscular" in prompt) and ("woman" in prompt):
-      prompt = "a man imprisoned in jail"
       steps = 1
     if ("bodybuild" in prompt) and ("girl" in prompt):
-      prompt = "a man imprisoned in jail"
       steps = 1
     if ("bodybuild" in prompt) and ("woman" in prompt):
-      prompt = "a man imprisoned in jail"
       steps = 1
     if ("strong" in prompt) and ("girl" in prompt):
-      prompt = "a man imprisoned in jail"
       steps = 1
     if ("strong" in prompt) and ("woman" in prompt):
-      prompt = "a man imprisoned in jail"
       steps = 1
     p = StableDiffusionProcessingTxt2Img(
         sd_model=shared.sd_model,
